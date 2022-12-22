01ee7e979edc3ec5b5feb6cfce26bd417d9a4c5d
khalifa <mednaceur199530@gmail.com>
2022-12-23 00:46:03 +0100

    this commit will add Product in db json

--- src/app/Pages/add-product/add-product.component.ts
+++ src/app/Pages/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/core/Product';
+import { ProductsServiceService } from 'src/app/services/products-service.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-
-  constructor() { }
+  product:Product=new Product();
+  constructor(private _serviceProduct: ProductsServiceService) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  add(){
+    this._serviceProduct.addProduct(this.product).subscribe(()=>alert('Product Succefully added'))
+  }
 }
