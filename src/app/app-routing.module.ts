2a2908bff05a8e7aa957de458fa5264a3c15d2d6
khalifa <mednaceur199530@gmail.com>
2022-12-22 23:15:16 +0100

    this commit will make routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './Pages/add-product/add-product.component';
+import { ListProductsComponent } from './Pages/list-products/list-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:ListProductsComponent},
+  {path:'addProduct',component:AddProductComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
