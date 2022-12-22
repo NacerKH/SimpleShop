392987dcab9f4be53356681797998ebc4e4c8a2a
khalifa <mednaceur199530@gmail.com>
2022-12-22 13:02:18 +0100

    make layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule,
